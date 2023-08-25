f9c125ce48294ccb2cc0f4d0a1ed29054e5124af
faysalahammedchowdhury01 <faysalahammedchowdhury01@gmail.com>
2023-08-25 16:47:28 +0600

    Submission added

---
+++ Submissions/Codeforces/1862/A_Gift_Carpet.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// @author: Faaysal
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t; cin >> t;
+  while (t--) {
+    int n, m; cin >> n >> m;
+    char a[n][m];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        cin >> a[i][j];
+      }
+    }
+    string need = "vika";
+    int curr = 0;
+    for (int j = 0; j < m; j++) {
+      for (int i = 0; i < n; i++) {
+        if (a[i][j] == need[curr]) {
+          curr++;
+          break;
+        }
+      }
+    }
+    if (curr >= 4) {
+      cout << "YES\n";
+    }
+    else {
+      cout << "NO\n";
+    }
+  }
+
+  return 0;
+}
