a25b93546aa9e4883bd8f1001164e1bd23cc4af0
faaysal <faysalahammedchowdhury01@gmail.com>
2023-10-05 21:43:06 +0600

    Submission added

---
+++ Submissions/Others/Codes/B - Incinerate.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 9;
+int heal[N];
+
+struct ST {
+  int tree[4 * N], lazy[4 * N];
+  void push(int n, int b, int e) {
+    if(lazy[n] == 0) return;
+    tree[n] += lazy[n]; // change here
+    if(b != e) {
+      int l = n << 1, r = l + 1;
+      lazy[l] += lazy[n]; // change here
+      lazy[r] += lazy[n]; // change here
+    }
+    lazy[n] = 0;
+  }
+  void build(int n, int  b, int e) {
+    lazy[n] = 0;
+    if(b == e) {
+      tree[n] = heal[b]; // change here
+      return;
+    }
+    int mid = (b + e) >> 1, l = n << 1, r = l + 1;
+    build(l, b, mid);
+    build(r, mid + 1, e);
+    tree[n] = max(tree[l], tree[r]); // change here
+  }
+  void upd(int n, int b, int e, int i, int j, int x) {
+    push(n, b, e);
+    if(b > j || e < i) return;
+    if(b >= i && e <= j) {
+      lazy[n] += x; // change here
+      push(n, b, e);
+      return;
+    }
+    int mid = (b + e) >> 1, l = n << 1, r = l + 1;
+    upd(l, b, mid, i, j, x);
+    upd(r, mid + 1, e, i, j, x);
+    tree[n] = max(tree[l], tree[r]); // change here
+  }
+  int query(int n, int b, int e, int i, int j) {
+    push(n, b, e);
+    if(b > j || e < i) return 0; // return appropriate value
+    if(b >= i && e <= j) return tree[n];
+    int mid = (b + e) >> 1, l = n << 1, r = l + 1;
+    int L = query(l, b, mid, i, j);
+    int R = query(r, mid + 1, e, i, j);
+    return max(L, R); // change here
+  }
+} st;
+
+void solve() {
+  int n, k; cin >> n >> k;
+  for(int i = 1; i <= n; i++) {
+    cin >> heal[i];
+  }
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+  for(int i = 1; i <= n; i++) {
+    int x; cin >> x;
+    pq.push({x, i});
+  }
+
+  st.build(1, 1, n);
+
+  while(k > 0) {
+    st.upd(1, 1, n, 1, n, -k);
+    int idx = pq.top().second;
+    int val = pq.top().first;
+    while(!pq.empty() and st.query(1, 1, n, idx, idx) <= 0) {
+      pq.pop();
+      idx = pq.top().second;
+      val = pq.top().first;
+    }
+    k -= val;
+
+    if(st.query(1, 1, n, 1, n) <= 0) {
+      cout << "Yes\n";
+      return;
+    }
+  }
+
+  cout << "No\n";
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t = 1;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+
+  return 0;
+}
