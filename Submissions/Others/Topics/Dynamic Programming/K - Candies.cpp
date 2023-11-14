27d1d89883fc8c2b390eb1cf739a10babcc3d690
faaysal <faysalahammedchowdhury01@gmail.com>
2023-11-14 12:12:42 +0600

    Submission added

--- Submissions/Others/Topics/Dynamic Programming/K - Candies.cpp
+++ Submissions/Others/Topics/Dynamic Programming/K - Candies.cpp
@@ -2,19 +2,7 @@
 using namespace std;
 
 const int N = 105, K = 1e5 + 9, mod = 1e9 + 7;
-int n, k, a[N];
-
-int fun(int i, int remaining) {
-  if (i == n + 1) {
-    if (remaining == 0) return 1;
-    return 0;
-  }
-  int ans = 0;
-  for (int j = 0; j <= a[i]; j++) {
-    ans += fun(i + 1, remaining - j);
-  }
-  return ans;
-}
+int n, k, a[N], dp[N][K];
 
 int32_t main() {
   ios_base::sync_with_stdio(0);
@@ -25,7 +13,32 @@ int32_t main() {
     cin >> a[i];
   }
 
-  cout << fun(1, k) << '\n';
+  for (int i = n + 1; i >= 1; i--) {
+    for (int remaining = 0; remaining <= k; remaining++) {
+      if (i == n + 1) {
+        if (remaining == 0) dp[i][remaining] = 1;
+        else dp[i][remaining] = 0;
+      }
+      else {
+        int &ans = dp[i][remaining];
+        // ans = 0;
+        // for (int j = 0; j <= a[i]; j++) {
+        //   if (remaining >= j) {
+        //     ans += dp[i + 1][remaining - j];
+        //     ans %= mod;
+        //   }
+        // }
+        int l = max(0, remaining - a[i]), r = remaining;
+        ans = (dp[i + 1][r] - (l ? dp[i + 1][l - 1] : 0) + mod) % mod;
+      }
+    }
+    for (int j = 1; i != 1 and j <= k; j++) {
+      dp[i][j] += dp[i][j - 1];
+      dp[i][j] %= mod;
+    }
+  }
+
+  cout << dp[1][k] << '\n';
 
   return 0;
 }
