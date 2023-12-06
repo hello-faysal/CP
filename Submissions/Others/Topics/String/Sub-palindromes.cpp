158bcba25aac61ed853e8427f58221665c609cb4
faaysal <faysalahammedchowdhury01@gmail.com>
2023-12-06 23:24:24 +0600

    Submission added

---
+++ Submissions/Others/Topics/String/Sub-palindromes.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 9; // change here
+
+int power(long long n, long long k, const int mod) {
+  int ans = 1 % mod;
+  n %= mod;
+  if (n < 0) n += mod;
+  while (k) {
+    if (k & 1) ans = (long long) ans * n % mod;
+    n = (long long) n * n % mod;
+    k >>= 1;
+  }
+  return ans;
+}
+
+const int MOD1 = 127657753, MOD2 = 987654319;
+const int p1 = 137, p2 = 277;
+int ip1, ip2;
+pair<int, int> pw[N], ipw[N];
+void prec() {
+  pw[0] = {1, 1};
+  for (int i = 1; i < N; i++) {
+    pw[i].first = 1ll * pw[i - 1].first * p1 % MOD1;
+    pw[i].second = 1ll * pw[i - 1].second * p2 % MOD2;
+  }
+  ip1 = power(p1, MOD1 - 2, MOD1);
+  ip2 = power(p2, MOD2 - 2, MOD2);
+  ipw[0] = {1, 1};
+  for (int i = 1; i < N; i++) {
+    ipw[i].first = 1ll * ipw[i - 1].first * ip1 % MOD1;
+    ipw[i].second = 1ll * ipw[i - 1].second * ip2 % MOD2;
+  }
+}
+
+struct Hashing {
+  int n;
+  string s;
+  vector<pair<int, int>> hash_val;
+  vector<pair<int, int>> rev_hash_val;
+  Hashing() {}
+  Hashing(string _s) {
+    s = _s;
+    n = s.size();
+    hash_val.emplace_back(0, 0);
+    for (int i = 0; i < n; i++) {
+      pair<int, int> p;
+      p.first = (hash_val[i].first + 1ll * s[i] * pw[i].first % MOD1) % MOD1;
+      p.second = (hash_val[i].second + 1ll * s[i] * pw[i].second % MOD2) % MOD2;
+      hash_val.push_back(p);
+    }
+    rev_hash_val.emplace_back(0, 0);
+    for (int i = 0, j = n - 1; i < n; i++, j--) {
+      pair<int, int> p;
+      p.first = (rev_hash_val[i].first + 1ll * s[i] * pw[j].first % MOD1) % MOD1;
+      p.second = (rev_hash_val[i].second + 1ll * s[i] * pw[j].second % MOD2) % MOD2;
+      rev_hash_val.push_back(p);
+    }
+  }
+
+  pair<int, int> get_hash(int l, int r) { // 1 indexed
+    pair<int, int> ans;
+    ans.first = (hash_val[r].first - hash_val[l - 1].first + MOD1) * 1ll * ipw[l - 1].first % MOD1;
+    ans.second = (hash_val[r].second - hash_val[l - 1].second + MOD2) * 1ll * ipw[l - 1].second % MOD2;
+    return ans;
+  }
+  pair<int, int> rev_hash(int l, int r) { // 1 indexed
+    pair<int, int> ans;
+    ans.first = (rev_hash_val[r].first - rev_hash_val[l - 1].first + MOD1) * 1ll * ipw[n - r].first % MOD1;
+    ans.second = (rev_hash_val[r].second - rev_hash_val[l - 1].second + MOD2) * 1ll * ipw[n - r].second % MOD2;
+    return ans;
+  }
+  pair<int, int> get_hash() { // 1 indexed
+    return get_hash(1, n);
+  }
+  bool is_palindrome(int l, int r) {
+    return get_hash(l, r) == rev_hash(l, r);
+  }
+};
+
+string s;
+Hashing hash_s;
+int n;
+
+bool ok(int l, int r) {
+  return hash_s.is_palindrome(l, r);
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  prec(); // must include
+
+  cin >> s;
+  n = s.size();
+  hash_s = Hashing(s);
+
+  long long ans = 0;
+  for (int i = 1; i <= n; i++) {
+    int l = 0, r = min(n - i, i - 1), cnt = 1;
+    while (l <= r) {
+      int mid = (l + r) >> 1;
+      if (ok(i - mid, i + mid)) {
+        cnt = mid;
+        l = mid + 1;
+      }
+      else {
+        r = mid - 1;
+      }
+    }
+    ans += cnt + 1;
+  }
+  for (int i = 2; i <= n; i++) {
+    if (s[i - 1] == s[i - 2]) {
+      int l = 0, r = min(n - i, i - 1), cnt = 2;
+      while (l <= r) {
+        int mid = (l + r) >> 1;
+        if (ok(i - 1 - mid, i + mid)) {
+          cnt = mid;
+          l = mid + 1;
+        }
+        else {
+          r = mid - 1;
+        }
+      }
+      ans += cnt + 1;
+    }
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
