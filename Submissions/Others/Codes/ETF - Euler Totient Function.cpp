872366591917207ca85d47b16b898e7431525156
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-01-17 17:00:06 +0600

    added

--- Submissions/Others/Codes/ETF - Euler Totient Function.cpp
+++ Submissions/Others/Codes/ETF - Euler Totient Function.cpp
@@ -1,49 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// if we need phi(x) multiple times, then memoize it
+map<int, int> dp;
+int phi(int n) {
+  if (dp.count(n)) return dp[n];
+  int ans = n, m = n;
+  for (int i = 2; i * i <= m; i++) {
+    if (m % i == 0) {
+      while (m % i == 0) m /= i;
+      ans = ans / i * (i - 1);
+    }
+  }
+  if (m > 1) ans = ans / m * (m - 1);
+  return dp[n] = ans;
+}
+
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-
-  int t; cin >> t;
-  while(t--) {
-    int n; cin >> n;
-
-    // sol-1
-    // int ans = 1;
-    // for(int i = 2; i * i <= n; i++) {
-    //   while(n % i == 0) {
-    //     int p = i, pw = 1;
-    //     while(n % p == 0) {
-    //       pw *= p;
-    //       n /= p;
-    //     }
-    //     ans *= pw - (pw / p); // (p^e) - (p^(e-1))
-    //   }
-    // }
-    // if(n > 1) {
-    //   int p = n, pw = n;
-    //   ans *= pw - (pw / p);
-    // }
-    // cout << ans << '\n';
-
-    // sol-2 (n * ((p1 - 1 ) / p1) * ((p2 - 1 ) / p2))
-    int ans = n;
-    for(int i = 2; i * i <= n; i++) {
-      while(n % i == 0) {
-        int p = i;
-        while(n % p == 0) {
-          n /= p;
-        }
-        ans *= (p - 1) / (double)p;
-      }
-    }
-    if(n > 1) {
-      int p = n;
-      ans *= (p - 1) / (double)p;
-    }
-    cout << ans << '\n';
-  }
 
   return 0;
 }
--- Submissions/Others/Codes/sieve phi.cpp
+++ Submissions/Others/Codes/sieve phi.cpp
@@ -30,7 +30,7 @@ void sieve_phi() {
   }
 }
 
-int main() {
+int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
