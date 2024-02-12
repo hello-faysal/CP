23fc099d5b515d61bdda7e1a4773a82fe81dfc69
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-02-12 21:31:55 +0600

    added

--- Submissions/Others/Topics/Number Theory/sieve.cpp
+++ Submissions/Others/Topics/Number Theory/sieve.cpp
@@ -17,10 +17,6 @@ void sieve() {
 }
 
 int main() {
-#ifndef ONLINE_JUDGE
-  freopen("input.txt","r",stdin);
-  freopen("output.txt","w",stdout);
-#endif
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
