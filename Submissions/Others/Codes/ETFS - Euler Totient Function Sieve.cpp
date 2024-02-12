486f046118d17867268e71bed208663c8223a58a
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-02-13 02:13:36 +0600

    added

--- Submissions/Others/Codes/ETFS - Euler Totient Function Sieve.cpp
+++ Submissions/Others/Codes/ETFS - Euler Totient Function Sieve.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
+void phi_1_to_n(int n) {
+  vector<int> phi(n + 1);
+  for (int i = 0; i <= n; i++)
+    phi[i] = i;
 
-int main() {
+  for (int i = 2; i <= n; i++) {
+    if (phi[i] == i) {
+      for (int j = i; j <= n; j += i)
+        phi[j] -= phi[j] / i;
+    }
+  }
+}
+
+
+int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
